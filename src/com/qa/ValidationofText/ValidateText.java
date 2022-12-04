08d311b9ce710b5313523c189ec926854ce1cc8c
gsdhami92 <gsingh3931@gmail.com>
2022-12-04 15:31:44 -0500

    2nd commit

--- src/com/qa/ValidationofText/ValidateText.java
+++ src/com/qa/ValidationofText/ValidateText.java
@@ -10,25 +10,24 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class ValidateText {
-@Test
-	public void validateGoogleText() {
-		
-	WebDriverManager.chromedriver().setup();
-	  WebDriver driver = new ChromeDriver();
-	  driver.get("https://www.google.com");
-	  driver.manage().window().maximize();
-	  
-	  driver.findElement(By.name("q")).sendKeys("fifa world cup 2022");
-	  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-	  
-	  WebElement firstLink = driver.findElement(By.xpath("//div[@class='ofy7ae'][1]"));
-	  String actualText = firstLink.getText();
-	  String expected = "FIFA World Cup Qatar 2022™";
-	  Assert.assertTrue(actualText.equalsIgnoreCase(expected),
-	  "Expected " +expected+ " but found" + actualText );
-	}
-	 }
-	
-	
 
+
+public class ValidateText {
+	@Test
+	public void validateGoogleText() {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("fifa world cup 2022");
+		search.sendKeys(Keys.ENTER);
+
+		WebElement firstLink = driver.findElement(By.xpath("//div[@class='ofy7ae'][1]"));
+		String actualText = firstLink.getText();
+		String expected = "FIFA World Cup Qatar 2022™";
+		Assert.assertTrue(actualText.equalsIgnoreCase(expected), "Expected " + expected + " but found" + actualText);
+	}
+}
